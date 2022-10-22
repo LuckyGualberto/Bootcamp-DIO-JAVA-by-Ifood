91f186866dacee1a6dc0aca7e627989f6e52d268
LuckyGualberto <108437016+LuckyGualberto@users.noreply.github.com>
2022-10-22 13:41:42 -0300

    Create Desafio3.java

---
+++ Desafio3.java
@@ -0,0 +1,28 @@
+// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.  
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner numero = new Scanner(System.in);
+        int N = numero.nextInt ();
+        int resultado = somatorio (N);
+        System.out.println(resultado);
+        //TODO: Imprima o somatório de N (utilize o método "somatorio").
+    }
+
+    /**
+     * Método útil que calcula o somatório de um número usando recursividade.
+     */
+    static int somatorio(int numero) {
+        if (numero == 0) {
+            return 0;
+        } else {
+            return numero + somatorio(numero - 1);
+        }
+    }
+}
